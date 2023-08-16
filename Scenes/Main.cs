d19ab5a247ac49be624f4a0653a22ed4cf02b73c
Will Roberts <willroberts3@protonmail.com>
2023-08-16 13:32:02 +0200

    Connects grid-engine to mouse input

--- Scenes/Main.cs
+++ Scenes/Main.cs
@@ -2,18 +2,27 @@ using Godot;
 
 public partial class Main : Node2D
 {
+	[Export]
+	public TileMap HighlightTiles;
+
+	[Export]
+	public TileMap PathTiles;
+
+	private Grid _grid = ResourceLoader.Load("res://Resources/Grid.tres") as Grid;
+	private readonly Board _gameboard = new();
+	private readonly BoardLayer _unitLayer = new();
+	private Vector2I _hoveredCell = Vector2I.Zero;
+
 	// FIXME: Move this.
 	private readonly string _playerTexture = "Assets/TinyDungeon/Tiles/tile_0097.png";
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		SpawnPlayer();
-	}
+		_unitLayer.HighlightTiles = HighlightTiles;
+		_unitLayer.PathTiles = PathTiles;
+		_gameboard.AddLayer("units", _unitLayer);
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		SpawnPlayer();
 	}
 
 	private void SpawnPlayer()
@@ -23,6 +32,31 @@ public partial class Main : Node2D
 		else { startPosition = new(1, 1); }
 
 		Texture2D tex = ResourceLoader.Load(_playerTexture) as Texture2D;
-		AddChild(new Player(startPosition, tex));
+		Player player = new(startPosition, tex);
+		_unitLayer.MoveFinished += player.OnMoved;
+		_unitLayer.Add(player, player.GetCell());
+		AddChild(player);
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		// Handle mouse click / touch.
+		if (@event is InputEventMouseButton btn && btn.ButtonIndex == MouseButton.Left && btn.Pressed)
+		{
+			_unitLayer.HandleClick(_grid.ScreenToGrid(btn.Position));
+			GetViewport().SetInputAsHandled();
+			return;
+		}
+
+		// Handle mouse motion.
+		if (@event is InputEventMouseMotion evt)
+		{
+			Vector2I hoveredCell = _grid.Clamp(_grid.ScreenToGrid(evt.Position));
+			if (hoveredCell.Equals(_hoveredCell)) { return; }
+			_hoveredCell = hoveredCell;
+			_unitLayer.HandleHover(_hoveredCell);
+			GetViewport().SetInputAsHandled();
+			return;
+		}
 	}
 }
