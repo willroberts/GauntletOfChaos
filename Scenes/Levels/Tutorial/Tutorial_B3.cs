b4721bc9b65df7821f4543635b4470969cf7f003
Will Roberts <willroberts3@protonmail.com>
2023-08-19 16:09:17 +0200

    Cleans up Tutorial_B3 tiles

--- Scenes/Levels/Tutorial/Tutorial_B3.cs
+++ Scenes/Levels/Tutorial/Tutorial_B3.cs
@@ -17,10 +17,10 @@ public partial class Tutorial_B3 : Level
 		});
 
 		SetTerrainTiles(new() {
-			// Top-left wall.
+			// Top wall, first half.
 			new(0, 0), new(1, 0), new(2, 0), new(3, 0), new(4, 0), new(5, 0), new(6, 0), new(7, 0), new(8, 0),
 
-			// Top-right wall.
+			// Top wall, second half.
 			new(11, 0), new(12, 0), new(13, 0), new(14, 0), new(15, 0), new(16, 0), new(17, 0), new(18, 0), new(19, 0),
 
 			// Gate enclosure.
@@ -32,8 +32,13 @@ public partial class Tutorial_B3 : Level
 			// Right wall.
 			new(19, 1), new(19, 2), new(19, 3), new(19, 4), new(19, 5), new(19, 6), new(19, 7), new(19, 8), new(19, 9), new(19, 10), new(19, 11),
 
-			// Bottom wall.
-			new(1, 11), new(2, 11), new(3, 11), new(4, 11), new(5, 11), new(6, 11), new(7, 11), new(8, 11), new(9, 11), new(10, 11),
+			// Bottom wall, first half.
+			new(1, 11), new(2, 11), new(3, 11), new(4, 11), new(5, 11), new(6, 11), new(7, 11), new(8, 11),
+
+			// Closed doors.
+			new(9, 11), new(10, 11),
+
+			// Bottom wall, second half.
 			new(11, 11), new(12, 11), new(13, 11), new(14, 11), new(15, 11), new(16, 11), new(17, 11), new(18, 11)
 		});
 
