0b9a1053157ff24ba60fac2ebf48c8e5d2d502c6
Will Roberts <willroberts3@protonmail.com>
2023-08-19 15:07:16 +0200

    Fixes inheritance

--- Scenes/Occupants/Player.cs
+++ Scenes/Occupants/Player.cs
@@ -3,26 +3,20 @@ using Godot;
 [GlobalClass]
 public partial class Player : Unit, IOccupant
 {
-    private readonly Grid _grid = ResourceLoader.Load("res://Resources/Grid.tres") as Grid;
-    private Vector2I _cell = Vector2I.Zero;
-    private Texture2D _texture;
-    private bool _isInCombat = false;
+	private readonly Grid _grid = ResourceLoader.Load("res://Resources/Grid.tres") as Grid;
+	private bool _isInCombat = false;
 
-    public Player(Vector2I cell, Texture2D texture) : base(cell, texture)
-    {
-        _cell = cell;
-        _texture = texture;
-    }
+	public Player(Vector2I cell, Texture2D texture) : base(cell, texture) { }
 
-    public override int GetRange()
-    {
-        if (_isInCombat) { return 3; }
-        return _grid.Size.X * _grid.Size.Y; // Practically unlimited.
-    }
+	public override int GetRange()
+	{
+		if (_isInCombat) { return 3; }
+		return _grid.Size.X * _grid.Size.Y; // Practically unlimited.
+	}
 
-    // TODO: Set this using BeginCombat / EndCombat signals.
-    public void SetIsInCombat(bool value)
-    {
-        _isInCombat = value;
-    }
+	// TODO: Set this using BeginCombat / EndCombat signals.
+	public void SetIsInCombat(bool value)
+	{
+		_isInCombat = value;
+	}
 }
--- Scenes/Occupants/Unit.cs
+++ Scenes/Occupants/Unit.cs
@@ -3,20 +3,14 @@ using Godot;
 public partial class Unit : Occupant
 {
 	private readonly Grid _grid = ResourceLoader.Load("res://Resources/Grid.tres") as Grid;
-	private Vector2I _cell = Vector2I.Zero;
 
-	// Constructing a Unit sets its basic property values.
-	public Unit(Vector2I cell, Texture2D texture) : base(cell, texture)
-	{
-		GD.Print("Creating Unit at ", cell);
-		_cell = cell;
-		if (texture != null) { AddSprite(texture); }
-	}
+	public Unit(Vector2I cell, Texture2D texture) : base(cell, texture) { }
 
 	// When the Unit finishes moving, update its location and position.
 	public void OnMoved(Vector2I newCell)
 	{
-		_cell = newCell;
+		SetCell(newCell);
 		Position = _grid.GridToScreen(newCell);
+		GD.Print("OnMoved: Position set to ", Position);
 	}
 }
