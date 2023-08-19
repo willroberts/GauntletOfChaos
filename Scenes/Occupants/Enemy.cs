0cf4e722ca184c44d5318f3c147cd2f3e8f0483c
Will Roberts <willroberts3@protonmail.com>
2023-08-19 20:55:48 +0200

    Starts to add some AI code

--- Scenes/Occupants/Enemy.cs
+++ Scenes/Occupants/Enemy.cs
@@ -3,20 +3,26 @@ using Godot;
 [GlobalClass]
 public partial class Enemy : Unit, IOccupant
 {
-    private Vector2I _cell = Vector2I.Zero;
-    private Texture2D _texture;
-    private bool _isInCombat = false;
+	public Enemy(Vector2I cell, Texture2D texture) : base(cell, texture) { }
 
-    public Enemy(Vector2I cell, Texture2D texture) : base(cell, texture)
-    {
-        _cell = cell;
-        _texture = texture;
-    }
+	// TODO: Create per-Enemy Resources which contain these stats.
+	public override int GetRange() { return 4; }
 
-    public override int GetRange() { return 4; }
+	// Basic AI flow: Ascertain -> Approach -> Ability -> Attack
+	public void ProcessTurn()
+	{
+		Vector2I targetCell = Vector2I.Zero;
+		Move(targetCell);
+		Attack();
+	}
 
-    public void SetIsInCombat(bool value)
-    {
-        _isInCombat = value;
-    }
+	public void Move(Vector2I targetCell)
+	{
+
+	}
+
+	public void Attack()
+	{
+
+	}
 }
