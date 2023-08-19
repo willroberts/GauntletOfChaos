72bf64962957e23df437363623abcdb47a59e029
Will Roberts <willroberts3@protonmail.com>
2023-08-19 14:44:13 +0200

    Iterates on levels

--- Scenes/Levels/Tutorial/Tutorial_A2.cs
+++ Scenes/Levels/Tutorial/Tutorial_A2.cs
@@ -1,15 +1,29 @@
 using Godot;
-using System;
 
-public partial class Tutorial_A2 : TileMap
+public partial class Tutorial_A2 : Level
 {
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GD.Print("Tutorial_A2 ready");
+		// Trigger tutorial message for this room.
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void Initialize()
 	{
+		SetPlayerStart(new(19, 6));
+
+		SetTerrainTiles(new() {
+            // Top wall: (13, 2) to (19, 2).
+            new(13, 2), new(14, 2), new(15, 2), new(16, 2), new(17, 2), new(18, 2), new(19, 2),
+
+            // Left wall: (13, 3) to (13, 8).
+            new(13, 3), new(13, 4), new(13, 5), new(13, 6), new(13, 7), new(13, 8),
+
+            // Bottom wall: (13, 9) to (19, 9).
+            new(13, 9), new(14, 9), new(15, 9), new(16, 9), new(17, 9), new(18, 9), new(19, 9),
+
+            // Right wall: (19, 3) to (19, 4) and (19, 7) to (19, 8).
+            new(19, 3), new(19, 4), new(19, 7), new(19, 8)
+		});
 	}
 }
