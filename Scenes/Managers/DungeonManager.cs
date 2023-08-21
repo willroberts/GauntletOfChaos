d93d987533b1e94f0b89a4e385899eeae657d141
Will Roberts <willroberts3@protonmail.com>
2023-08-21 16:54:44 +0200

    Adds DungeonManager

---
+++ Scenes/Managers/DungeonManager.cs
@@ -0,0 +1,34 @@
+using Godot;
+using Godot.Collections;
+
+public partial class RoomState : RefCounted
+{
+	private string _serialized;
+
+	public void SerializeBoardLayer(BoardLayer layer)
+	{
+		// FIXME: Actually serialize.
+		_serialized = layer.ToString();
+	}
+}
+
+// DungeonManager is responsible for tracking the state of rooms in a Dungeon.
+public partial class DungeonManager : Node2D
+{
+	private Dictionary<string, RoomState> _stateMap;
+
+	public RoomState LoadRoomState(string room)
+	{
+		if (!_stateMap.ContainsKey(room))
+		{
+			GD.Print("Error: Room ", room, " does not exist in the state map!");
+			return null;
+		}
+		return _stateMap[room];
+	}
+
+	public void SaveRoomState(string room, RoomState state)
+	{
+		_stateMap[room] = state;
+	}
+}
