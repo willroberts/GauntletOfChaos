5b1d0c491bf8e4367160975685b17d5e38b1b19f
Will Roberts <willroberts3@protonmail.com>
2023-08-23 11:45:10 +0200

    Merges ActionManager into PlayerManager

--- Scenes/Managers/PlayerManager.cs
+++ Scenes/Managers/PlayerManager.cs
@@ -1,6 +1,36 @@
 using Godot;
+using Godot.Collections;
 
 public partial class PlayerManager : Node2D
 {
+	public Dictionary<string, Array<Vector2I>> GetActions(System.Collections.Generic.Dictionary<Vector2I, IOccupant> neighbors)
+	{
+		Dictionary<string, Array<Vector2I>> actions = new()
+		{
+			["attack"] = new(),
+			["chest"] = new(),
+			["switch"] = new()
+		};
 
+		foreach ((Vector2I cell, IOccupant neighbor) in neighbors)
+		{
+			if (neighbor is Enemy)
+			{
+				actions["attack"].Add(cell);
+				GD.Print("Debug[ActionManager]: You can attack an enemy at ", cell);
+			}
+			else if (neighbor is Chest)
+			{
+				actions["chest"].Add(cell);
+				GD.Print("Debug[ActionManager]: You can open a chest at ", cell);
+			}
+			else if (neighbor is Switch)
+			{
+				actions["switch"].Add(cell);
+				GD.Print("Debug[ActionManager]: You can activate a switch at ", cell);
+			}
+		}
+
+		return actions;
+	}
 }
